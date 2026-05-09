9241040b5e4d5103935d06df741959063c9a96ff
Sudipro-del <Sudipro-del@users.noreply.github.com>
2026-05-09 13:09:50 +0000

    deploy: 6698abeec26a710fda4dce0fa4c3858f4ce49927

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.options = {
-    "domain": "5gvci.com",
-    "zoneId": 10240042
+    "domain": "3nbf4.com",
+    "zoneId": 10984898
 }
 self.lary = ""
-importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
+importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
